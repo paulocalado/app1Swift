53d67d12ad1c294447e0b33443ff5a394c02a87a
Paulo Henrique Calado Aoun <paulo_calado@live.com>
2018-04-07 13:52:12 -0300

    preparando interface
    Primeiro commit com apenas a interface d app de idade do cachorro

---
+++ app1/app1/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  app1
+//
+//  Created by Paulo Calado on 06/04/18.
+//  Copyright © 2018 Codgin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Hello IOS")
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
